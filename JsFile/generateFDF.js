a087bc701c6f7ea61c1b2800093e259e95cb3dde
Betterteng <betterteng@gmail.com>
2017-09-06 14:19:41 +1000

    page2

--- JsFile/generateFDF.js
+++ JsFile/generateFDF.js
@@ -12,8 +12,8 @@ var entryPageOne = {firstTable: [], secondTable: [], thirdTable: []};
 function generatePDF() {
     console.log('滕施男太帅了！');
 
-    //drawPageOne();
-    //doc.addPage();
+    drawPageOne();
+    doc.addPage();
     drawPageTwo();
 
     // Save the PDF file
@@ -106,7 +106,7 @@ function drawPageOne() {
      *  Fill user's input - if you want to only draw the skeletons such as table borders,
      *  comment these methods below.
      * */
-    fillUserInputPageOne();
+    //fillUserInputPageOne();
 }
 
 function fillUserInputPageOne() {
@@ -171,6 +171,29 @@ function drawPageTwo() {
 
     // Explanation
     setExplanationStyle();
+    doc.text(15, 25, 'This Report is prepared by Archicentre Australia – a division of ArchiAdvisory Pty Ltd – ' +
+        'and in accordance with Australian \nStandard 4349.1-2007 Assessment of Buildings Part 1: Pre-purchase ' +
+        'Inspections – Residential Buildings and any other \nAustralian Standards and definitions cited in the ' +
+        'Terms and Conditions. \n\n' +
+        'This Report is a subjective assessment prepared by the appraising architect on a visual assessment of the condition of \n' +
+        'the reasonably accessible parts of the property and on the basis of the prevailing structural, soil and weather conditions \n' +
+        'at the time of the assessment and does not cover enquiries of councils or other authorities. It is not a certificate of \n' +
+        'compliance for the property within the requirements of any Act, regulation, ordinance or local by-law. \n\n' +
+        'Prolonged periods of wet or dry weather may cause structural changes to the property as described in the Property \n' +
+        'Maintenance Guide which you can download from the link found in the body of your Report and in the Report cover letter. \n\n' +
+        'This Report will not disclose defects in inaccessible areas, defects that are concealed and/or not reasonably visible, \n' +
+        'defects that may be apparent in other weather conditions or defects that have not yet arisen. \n\n' +
+        'This Report is not a rigorous assessment of all building elements and does not cover all maintenance items, particularly \n' +
+        'those such as jamming doors, windows or catches, decorative finishes and hairline or slight cracks.  This is in \n' +
+        'accordance with Category 0 and 1 of Appendix C –AS 2870-2011. \n\n' +
+        'This Report is not a pest inspection. Archicentre Australia recommends that a pest inspection be carried out on all \n' +
+        'properties being considered for purchase. Customers wishing to have a full pest inspection should contact Archicentre \n' +
+        'Australia to arrange a separate pest inspection. Archicentre Australia’s Timber Pest Inspections are undertaken by highly \n' +
+        'qualified, independent and authorised inspectors.  Archicentre Australia does not sell pest control services so you can \n' +
+        'trust that you will not be recommended treatments that you do not need. \n\n' +
+        'The assessment assumes that the existing use of the property will continue and the Report is prepared on that basis. As \n' +
+        'such, the assessment will not assess the fitness of the property for any other intended purpose.  We advise you to verify \n' +
+        'any proposed change in use with the relevant authorities.');
 
     /**
      *  Fill user's input - if you want to only draw the skeletons such as table borders,
